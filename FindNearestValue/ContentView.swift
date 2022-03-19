f78a8e886340abb8aa0aed6da3b1b350d2dc45d0
slambeable <41022916+slambeable@users.noreply.github.com>
2022-03-19 23:02:27 +0300

    Initial Commit

---
+++ FindNearestValue/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FindNearestValue
+//
+//  Created by Андрей Евдокимов on 19.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FindNearestValue/FindNearestValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindNearestValueApp.swift
+//  FindNearestValue
+//
+//  Created by Андрей Евдокимов on 19.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FindNearestValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
